bc294bd563b6ef3e3429d7b4a08ae1d68428829a
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-09-12 09:18:11 -0700

    store us json locally

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -17,10 +17,11 @@ svg.append("rect")
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
+    .on("click", clicked);
 
 var g = svg.append("g");
 
-d3.json("/mbostock/raw/4090846/us.json", function(error, us) {
+d3.json("us.json", function(error, us) {
   if (error) throw error;
 
   g.append("g")
@@ -36,3 +37,28 @@ d3.json("/mbostock/raw/4090846/us.json", function(error, us) {
       .attr("id", "state-borders")
       .attr("d", path);
 });
+
+function clicked(d) {
+  var x, y, k;
+
+  if (d && centered !== d) {
+    var centroid = path.centroid(d);
+    x = centroid[0];
+    y = centroid[1];
+    k = 4;
+    centered = d;
+  } else {
+    x = width / 2;
+    y = height / 2;
+    k = 1;
+    centered = null;
+  }
+
+  g.selectAll("path")
+      .classed("active", centered && function(d) { return d === centered; });
+
+  g.transition()
+      .duration(750)
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+      .style("stroke-width", 1.5 / k + "px");
+}
