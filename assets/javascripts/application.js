0936cab826c27403b211ed2c575b069420173125
Connor Paul Stauffer <connor.paul.stauffer@gmail.com>
2015-09-13 10:14:17 -0700

    add salary distribution to data json

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -5,29 +5,48 @@ function initialize () {
       .attr("width", width)
       .attr("height", height);
 
-  d3.json("united_states_paths.json", function (error, paths) {
-    if (error) throw error;
-    for (var i = 0; i < paths.length; i++) {
-      paths[i]
-      svg.selectAll(".state")
-			   .data(paths).enter().append("path").attr("class","state")
-         .attr("d",function(d){ return d.d;})
-			   .style({"fill": "grey", "stroke": "white"})
-         .on("mouseover", handleMouseOver)
-         .on("mouseout", handleMouseOut)
-    }
-  });
 
   function renderMap (occupation) {
-    occupation = occupation || "All Occupations";
-    maxMin = findMaxMin(occupation);
+    var occupation = occupation || "All Occupations";
+    var stateData = getStateData(occupation);
+    var minMax = findMinMax(stateData);
+
+    d3.json("united_states_paths.json", function (error, paths) {
+      if (error) throw error;
+      // for (var i = 0; i < paths.length; i++) {
+      //   paths[i]
+      svg.selectAll(".state")
+			   .data(paths).enter().append("path").attr("class","state")
+         .attr("d", function (d) { return d.d; })
+			   .style({ "fill": "grey", "stroke": "white" })
+         .on("mouseover", handleMouseOver)
+         .on("mouseout", handleMouseOut)
+      // }
+    });
   };
 
-  function findMaxMin (occupation) {
+  function findMinMax (stateData) {
+    var min = null, max = null;
+
+    for (var i = 0; i < stateData.length; i++) {
+      var salary = parseInt(states[i]["average_salary"]);
+      if (!min || salary < min) { min = salary; }
+      if (!max || salary > max) { max = salary; }
+    }
+
+    return [min, max];
+  };
+
+  function getStateData (occupation) {
+    var data = [];
+
     d3.json("data.json", function (error, data) {
       if (error) throw error;
-      debugger;
+      data = data["occupations"][occupation]
+      if (!data) throw error;
     })
+
+    return data;
   };
 
   function handleMouseOver () {
--- data_script.rb
+++ data_script.rb
@@ -17,6 +17,22 @@ CSV.foreach("data.csv", { encoding: 'ISO-8859-1', headers: true, header_converte
   end
 end
 
+data[:occupations].each do |_, states|
+  min = states.min_by { |state| state[:average_salary].to_i }[:average_salary].to_i
+  max = states.max_by { |state| state[:average_salary].to_i }[:average_salary].to_i
+  states.each do |state|
+    if max = min
+      state[:distribution] = 0
+    else
+      begin
+        state[:distribution] = (state[:average_salary].to_i - min) / (max - min)
+      rescue
+        byebug
+      end
+    end
+  end
+end
+
 File.open("data.json", "w") do |f|
   f.write(data.to_json)
 end
